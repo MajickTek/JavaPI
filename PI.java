bae7d4e57892ff8d3a16cc739a8b18a118c3b1ae
Majick Tek <kingtmw2@gmail.com>
2017-11-20 16:07:49 -0500

    Update PI.java

--- PI.java
+++ PI.java
@@ -1,12 +1,12 @@
 import java.*;
-import java.math.*;
+import java.lang.math.*;
 
 public class PI {
 
     public static void main(String[] args) {
         // Prints Math.PI and the extended Math.PI to the terminal window.
         System.out.println("Standard Math.PI: "+ Math.PI);
-        System.out.println("Extended Math.PI:"+ java.math.BigDecimal(Math.PI));
+        System.out.println("Extended Math.PI:"+ java.lang.math.BigDecimal(Math.PI));
         System.out.println("Used java.math.BigDecimal to extend Math.PI to the system's legnth");
     }
 
