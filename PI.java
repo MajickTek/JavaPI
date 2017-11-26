3c34e557ae733ceca07f910350fe1880e2867956
Majick Tek <kingtmw2@gmail.com>
2017-11-26 15:16:58 -0500

    Update PI.java with PR by danbiocchi (see issues#1)

--- PI.java
+++ PI.java
@@ -1,11 +1,13 @@
-import java.*;
+//Changes by danbiocchi (https://github.com/danbiocchi/JavaPI/pull/1)
+
+import java.math.BigDecimal;
 
 public class PI {
 
     public static void main(String[] args) {
         // Prints Math.PI and the extended Math.PI to the terminal window.
         System.out.println("Standard Math.PI: "+ Math.PI);
-        System.out.println("Extended Math.PI: "+ java.lang.math.BigDecimal(Math.PI)); //this and java.math don't work
+        System.out.println("Extended Math.PI: "+ new BigDecimal(Math.PI)); //Creates a new big decimal with the value of Math.PI
         System.out.println("Used java.math.BigDecimal to extend Math.PI to the system's legnth");
     }
 
